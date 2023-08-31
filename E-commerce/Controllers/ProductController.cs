7048d38eac858fc9fd1fab9c46e4aba7bd2404d8
Fatma Mahmoud <mahmoudfatma3060@gmail.com>
2023-08-31 19:20:41 +0200

    product controller index iaction

---
+++ E-commerce/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using E_commerce.Models;
+using E_commerce.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        EcommerceCountext db = new EcommerceCountext();
+        
+        public IActionResult Index()
+        {
+            ProductJoinSellerViewModel vm = new ProductJoinSellerViewModel();
+            vm.Products = db.Products.ToList();
+            vm.Sellers = db.Sellers.ToList();
+
+            return View(vm);
+        }
+    }
+}
